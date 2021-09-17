c2287827828866ee475f55193cd8faf8d191ecd2
Nabin Hyanmikha <Hyanmikhanabin@gmail.Com>
2021-09-17 10:23:32 +0545

    Delete day_3 directory

--- day_3/setup.py
+++
@@ -1,4 +0,0 @@
-from os import name
-from setuptools import setup, find_packages, version
-
-setup(name = 'day_3', version = '1.0', packages = find_packages())
--- day_3/src/sql/queries/delete_raw_employees.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM etl.raw_employee;
